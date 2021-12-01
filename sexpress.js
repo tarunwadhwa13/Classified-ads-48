91a9eecce513bd3827cb3b591af7959a0602d31c
bacloud14 <bacloud14@gmail.com>
2021-12-01 19:32:08 +0100

    fixing middlewares

--- sexpress.js
+++ sexpress.js
@@ -66,8 +66,10 @@ app.get('/i18n/:locale', (req, res) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+
+
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 // app.use(cookieParser(process.env.SESSION_SECRET));
 app.use('/so-c', express.static(path.join(__dirname, 'app_so-cards')))
